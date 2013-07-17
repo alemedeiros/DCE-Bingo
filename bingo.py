15bd3764bd9316bdbb09ca34ccb58f4388ebaff8
alexandre medeiros <alexandre.medeiros@students.ic.unicamp.br>
2013-07-17 00:11:26 -0300

    gerando arquivo bingo.tex

--- bingo.py
+++ bingo.py
@@ -8,6 +8,7 @@ import datetime
 lista = []
 final = []
 html = True
+latex = True
 
 # abre base de palavras
 arquivo = open('palavras.txt','r')
@@ -43,25 +44,65 @@ if html:
     saida = open('bingo.html','w')
 
     # insere cabeçalho
-    saida.write('<html><head><title>WEB BINGO DO DCE!</title></head><body>')
-    saida.write('<h1>Bingo do DCE</h1>')
+    saida.write('<html><head><title>Bingo do DCE</title></head><body>\n')
+    saida.write('<h1>Bingo do DCE</h1>\n')
     hoje = datetime.date.today()
     saida.write('<p>Cartela gerada %s</p>' % hoje)
-    saida.write('<p> Seu nome: ___________________________<br /><br />')
-    saida.write('    Seu RA: _____________________________</p><br />')
+    saida.write('<p> Seu nome: ___________________________<br /><br />\n')
+    saida.write('    Seu RA: _____________________________</p><br />\n')
 
     # símbolo especial para o centro da cartela
     especial = '<img src="bingo.jpg">'
 
     # cartela
-    saida.write('<table border="1">')
-    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>' %(final[0],final[1],final[2],final[3],final[4]))
-    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>' %(final[5],final[6],final[7],final[8],final[9]))
-    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>' %(final[10],final[11],especial,final[12],final[13]))
-    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>' %(final[14],final[15],final[16],final[17],final[18]))
-    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>' %(final[19],final[20],final[21],final[22],final[23]))
-    saida.write('</table>')
-    saida.write('</body></html>')
+    saida.write('<table border="1">\n')
+    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>\n' %(final[0],final[1],final[2],final[3],final[4]))
+    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>\n' %(final[5],final[6],final[7],final[8],final[9]))
+    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>\n' %(final[10],final[11],especial,final[12],final[13]))
+    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>\n' %(final[14],final[15],final[16],final[17],final[18]))
+    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>\n' %(final[19],final[20],final[21],final[22],final[23]))
+    saida.write('</table>\n')
+    saida.write('</body></html>\n')
+
+    saida.close()
+
+if latex:
+    # abre arquivo de saída
+    saida = open('bingo.tex','w')
+
+    # insere cabeçalho
+    saida.write('\\documentclass[12pt,a4paper]{article}\n')
+    saida.write('\\usepackage{fullpage}\n')
+    saida.write('\\usepackage[brazilian]{babel}\n')
+    saida.write('\\usepackage[utf8]{inputenc}\n')
+    saida.write('\\usepackage[T1]{fontenc}\n')
+    saida.write('\\usepackage{indentfirst}\n')
+    saida.write('\n')
+    saida.write('\\usepackage{graphicx}\n')
+    saida.write('\\usepackage{array}\n')
+    saida.write('\n')
+    saida.write('\\title{Bingo do DCE}\n')
+    saida.write('\\author{Seu nome: \\rule{5cm}{0.1mm}\\\\\n')
+    saida.write('        Seu RA:  \\rule{2.5cm}{0.1mm}}\n')
+    saida.write('\\date{Cartela gerada \\today}\n')
+    saida.write('\n')
+    saida.write('\\begin{document}\n')
+    saida.write('\\maketitle\n')
+    saida.write('\n')
+    saida.write('\\centering\n')
+
+    # símbolo especial para o centro da cartela
+    especial = '\\includegraphics[height=12pt]{unicamp}'
+
+    # cartela
+    saida.write('\\begin{tabular}{|c|c|c|c|c|}\\hline\n')
+    saida.write('%s & %s & %s & %s & %s \\\\\\hline\n' % (final[0],final[1],final[2],final[3],final[4]))
+    saida.write('%s & %s & %s & %s & %s \\\\\\hline\n' % (final[5],final[6],final[7],final[8],final[9]))
+    saida.write('%s & %s & %s & %s & %s \\\\\\hline\n' % (final[10],final[11],especial,final[12],final[13]))
+    saida.write('%s & %s & %s & %s & %s \\\\\\hline\n' % (final[14],final[15],final[16],final[17],final[18]))
+    saida.write('%s & %s & %s & %s & %s \\\\\\hline\n' % (final[19],final[20],final[21],final[22],final[23]))
+    saida.write('\\end{tabular}\n')
+    saida.write('\\end{document}\n')
 
     saida.close()
 
