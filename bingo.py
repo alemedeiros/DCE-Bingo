81f828c5365dab510731dddc25b1466a03339ff4
alexandre medeiros <alexandre.medeiros@students.ic.unicamp.br>
2013-07-16 23:20:59 -0300

    modificando formato da cartela e estrutura do código

--- bingo.py
+++ bingo.py
@@ -1,55 +1,70 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import random
+import datetime
 
 # inicializa listas
-lista=[]
-final=[]
+lista = []
+final = []
+html = True
 
 # abre base de palavras
-arquivo=open("palavras.txt","r")
-resultado=open("bingo.html","w")
+arquivo = open('palavras.txt','r')
 
 # percorre arquivo de palavras
-while True:
-        palavra=arquivo.readline()
-# se chegar ao final do arquivo, interrompe
-        if len(palavra)==0:
-                break
-# enquanto ler palavra valida, adiciona aa lista de palavras
-        finalp=len(palavra)-1
-        lista.append(palavra[:finalp])
+linhas = arquivo.readlines()
 
-# para escolher as 24 palavras da tabela
+for palavra in linhas:
+    # adiciona na lista de palavras, removendo o '\n'
+    lista.append(palavra[:-1])
+
+# verifica se há palavras o suficiente
+if len(lista) < 25:
+    print 'ERRO: poucas palavras no palavras.txt'
+    exit(1)
+
+# escolhe 24 palavras para tabela
+escolhidas = set()
 for contador in range (1,25):
-        # gera um numero randomico menor que o numero de palavras na lista
-        indice=int(len(lista)*random.random())
-        escolhida=lista[indice]
-        # Adiciona a palavra escolhida aa lista da cartela
-        final.append(escolhida)
-        # Remove a palavra da lista inicial, para evitar duplicata na cartela
-        lista.remove(escolhida)
+    # gera um número aleatório até encontrar uma palavra que não foi escolhida
+    while True:
+        indice = int(len(lista) * random.random())
+        if indice not in escolhidas:
+            break;
 
+    # adiciona a palavra escolhida na lista da cartela
+    final.append(lista[indice])
+    # marca palavra como escolhida
+    escolhidas.add(indice)
 
-#resultado.write('Content-type: text/html')
-#resultado.write('')
-resultado.write('<HTML><HEAD><TITLE>WEB BINGO DO DCE!</TITLE></HEAD><BODY>')
-resultado.write('<H1>Bingo do DCE</H1>')
-resultado.write('<H2>Gerador de cartelas</H2>')
-resultado.write('<P> Seu nome: ___________________________</P>')
-resultado.write('<P> Seu RA: _____________________________</P>')
+if html:
+    # abre arquivo de saída
+    saida = open('bingo.html','w')
 
-especial ='<IMG SRC="161590.jpg">'
+    # insere cabeçalho
+    saida.write('<html><head><title>WEB BINGO DO DCE!</title></head><body>')
+    saida.write('<h1>Bingo do DCE</h1>')
+    hoje = datetime.date.today()
+    saida.write('<p>Cartela gerada %s</p>' % hoje)
+    saida.write('<p> Seu nome: ___________________________<br /><br />')
+    saida.write('    Seu RA: _____________________________</p><br />')
 
-resultado.write('<P>Cartela gerada:</P>')
-resultado.write('<TABLE BORDER="1">')
-resultado.write('<TR><TD> %s </TD><TD> %s </TD><TD> %s </TD><TD> %s </TD><TD> %s </TD></TR>' %(final[0],final[1],final[2],final[3],final[4]))
-resultado.write('<TR><TD> %s </TD><TD> %s </TD><TD> %s </TD><TD> %s </TD><TD> %s </TD></TR>' %(final[5],final[6],final[7],final[8],final[9]))
-resultado.write('<TR><TD> %s </TD><TD> %s </TD><TD> %s </TD><TD> %s </TD><TD> %s </TD></TR>' %(final[10],final[11],especial,final[12],final[13]))
-resultado.write('<TR><TD> %s </TD><TD> %s </TD><TD> %s </TD><TD> %s </TD><TD> %s </TD></TR>' %(final[14],final[15],final[16],final[17],final[18]))
-resultado.write('<TR><TD> %s </TD><TD> %s </TD><TD> %s </TD><TD> %s </TD><TD> %s </TD></TR>' %(final[19],final[20],final[21],final[22],final[23]))
-resultado.write('</TABLE>')
-resultado.write('</BODY></HTML>')
+    # símbolo especial para o centro da cartela
+    especial = '<IMG SRC="161590.jpg">'
+
+    # cartela
+    saida.write('<table border="1">')
+    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>' %(final[0],final[1],final[2],final[3],final[4]))
+    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>' %(final[5],final[6],final[7],final[8],final[9]))
+    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>' %(final[10],final[11],especial,final[12],final[13]))
+    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>' %(final[14],final[15],final[16],final[17],final[18]))
+    saida.write('<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>' %(final[19],final[20],final[21],final[22],final[23]))
+    saida.write('</table>')
+    saida.write('</body></html>')
+
+    saida.close()
 
 arquivo.close()
-resultado.close()
+
+exit(0)
